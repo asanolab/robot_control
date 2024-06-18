257fb8f0dbe4800b828398d52816e18a932aac1c
Yuki Asano <yasano@g.ecc.u-tokyo.ac.jp>
2024-06-18 16:38:16 +0900

    add dobot_magician_interface

---
+++ robots/dobot_magician/dobot_magician_interface/scripts/test_manip_dobot_magician.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import rospy
+import math
+
+from dobot_magician_interface.dobot_magician_interface import DobotMagicianInterface
+
+
+def main():
+    rospy.init_node("test_manip_dobot_magician", anonymous=True)
+    ri = DobotMagicianInterface("arm")
+
+    # Initial Pose
+    rospy.loginfo("Moving to Initial Pose")
+    ri.fk([0.0, 0.0, 0.0, 0.0])
+
+    # Pose 1
+    rospy.loginfo("Moving to Pose 1")
+    ri.fk([40.0, 30.0, 20.0, 10.0])
+
+    # Pose 2
+    rospy.loginfo("Moving to Pose 2")
+    ri.ik_relative([0.0, 0.0, -0.05, 0.0, 0.0, 0.0])
+
+    # Pose 3
+    rospy.loginfo("Moving to Pose 3")
+    ri.fk([-40.0, 0.0, 0.0, 0.0])
+
+    # Pose 4
+    rospy.loginfo("Moving to Pose 4")
+    ri.ik_relative([0.05, 0.0, 0.0, 0.0, 0.0, 0.0])
+
+    # Back to Initial Pose
+    rospy.loginfo("Moving to Initial Pose again")
+    ri.fk([0.0, 0.0, 0.0, 0.0])
+
+
+    rospy.loginfo("End")
+
+
+if __name__ == '__main__':
+    main()
