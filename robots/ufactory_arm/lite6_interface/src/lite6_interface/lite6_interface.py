654ed12bf686d11fd05acac78f4a5d64b2ffb43d
Yuki Asano <yasano@g.ecc.u-tokyo.ac.jp>
2024-06-26 14:57:03 +0900

    enable to move real lite6 robot through lite6_interface

--- robots/ufactory_arm/lite6_interface/src/lite6_interface/lite6_interface.py
+++ robots/ufactory_arm/lite6_interface/src/lite6_interface/lite6_interface.py
@@ -3,13 +3,36 @@
 
 import rospy
 
-from robot_interface import RobotInterface
+from xarm_msgs.srv import Move
+
+from robot_interface.robot_interface import RobotInterface,deg2rad,rad2deg
 
 
 class Lite6Interface(RobotInterface):
-    def __init__(self, robot_name='Lite6'):
-        print('init')
+    def __init__(self, robot_name='lite6'):
         super().__init__(robot_name)
+        self.max_speed = 0.35  # [rad/s]
+        self.max_acc = 7       # [rad/s^2]
+        self.ip_time = 1       # [s]
 
-    #def move(self):
-    #    move;
+
+    def move_real_robot(self):
+        rospy.wait_for_service('/ufactory/move_joint')
+        try:
+            move_joint = rospy.ServiceProxy('/ufactory/move_joint', Move)
+            res = move_joint(self.target_angle, self.max_speed, self.max_acc, 0, 0)
+            '''
+            args (xarm_msgs/srv/Move.srv):
+            # pose: target coordinate.
+            # mvvelo: (value range: 0 ~ 1000) specified maximum velocity during execution. linear (0 to 1000 mm/s)  or angular (range 0~1000 for 0 to 3.14 rad/s) velocity.
+            # mvacc: specified maximum linear acceleration (mm/s^2) during execution. (angular acceleration is fixed currently).
+            # mvtime: currently do not have any special meaning, please just give it 0.
+            # PLEASE NOTE: after firmware version 1.5, For servo_cartesian motion, mvtime will be used as indicator of coordinate system. (0 for BASE coordinate, non-zero for TOOL coordinate)
+            # mvradii: this is special for move_ineb service, meaning the blending radius between 2 straight path trajectories, 0 for no blend.
+            '''
+            ret = res.ret
+            message = res.message
+
+            rospy.sleep(self.ip_time)  # need to change to interpolation time
+        except rospy.ServiceException as e:
+            print('Service call failed: %s' % e)
