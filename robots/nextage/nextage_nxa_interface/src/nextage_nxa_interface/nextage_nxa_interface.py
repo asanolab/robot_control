41e8603998ee273f7b102b9eaba7fc0f80022228
Yuki Asano <yuki.asano3206@gmail.com>
2025-11-10 19:19:11 +0900

    update nx_interface

--- robots/nextage/nextage_nxa_interface/src/nextage_nxa_interface/nextage_nxa_interface.py
+++ robots/nextage/nextage_nxa_interface/src/nextage_nxa_interface/nextage_nxa_interface.py
@@ -5,43 +5,88 @@
 # - authority is needed for write by changing display line to yellow (read is not need)
 # - u'' is not necessary in python3 (python2 needs)
 
+import socket
+
 ## NEXTAGE
 from omniORB import CORBA, any as ANY
 from NxApi import *
 
-#variables:
-# - thickness_counter
-# - micrometer_counter
-# - sample_assemble_counter
-# - sample_set_counter
-# - sample_disassemble_counter
+
+class Var:
+    def __init__(self, name):
+        self.name = name  # string
+        self.obj = None
+        return
+
+    def Connect(self, task):
+        self.obj = task.GetVariable(u"@GLOBAL_VARS/"+self.name, u"")
+        return
+
+    def Get(self):
+        val = ANY.from_any(self.obj.Execute(u"GetValues", ANY.to_any(None)))[0]
+        return val
+
+    def Set(self, val):
+        self.obj.Execute(u"SetValues",
+                         ANY.to_any([[[u"value", unicode(str(val))]]]))
+        return
+    pass
 
 
 class NextageNXAInterface():
-    def __init__(self, nx_ip="192.168.0.10", debug=True):
-        self.ipaddr_str = nx_ip
+    def __init__(self, ip="192.168.0.10", debug=False):
+        self.ipaddr_str = ip
         self.argv = ["-ORBdefaultWCharCodeSet", "UTF-16", "-ORBgiopMaxMsgSize", "104857600"]
+        self.task = None
+        self.skt = None
+        self.port = 5000
+        self.debug = debug
 
-        # Connect to the API servern
+
+    def setup(self, speed=100, target_task_name='task1'):
+        # Connect to the API server
         self.orb = CORBA.ORB_init(self.argv, CORBA.ORB_ID)
-        self.api = self.orb.string_to_object(
-            "corbaloc:iiop:1.2@%s:2809/RootControllerApi" % (self.ipaddr_str)
-        )
+        self.api = self.orb.string_to_object("corbaloc:iiop:1.2@%s:2809/RootControllerApi"%(self.ipaddr_str))
 
+        # Setup controller
         self.root_controller = self.api._narrow(RootNxController)
-
-        # Raising an exception if the maximum number of clients is reached
-        self.controller = self.root_controller.GetNxController("", "")
+        self.controller = self.root_controller.GetNxController(u"", u"")
+        self.whole_body = self.controller.GetNxObject(u"Robot", u"All")
 
         # Get Variable's variable values
         self.task = self.controller.GetNxObject(u"Task", u"")
         self.varlist= ANY.from_any(self.task.GetVariableNames(u"@GLOBAL_VARS"))
 
-        self.debug = debug
-
+        # print
+        print("Task Name: '%s'"%(self.task.Name))
         if self.debug:
-            print("Task Name: '%s'"%(self.task.Name))
             print("varlist: '%s'"%(self.varlist))
+
+        if (self.task.Name != target_task_name):
+            self.task.Execute(u"SetTask", ANY.to_any([[u"taskName", target_task_name]]))
+            print("Task has changed: '%s'"%(self.task.Name))
+            pass
+
+        #speed_var = controller.GetVariable(u"@SPEED", u"")
+        #speed = ANY.to_any([["value", speed]])
+        #speed_var.Execute(u"SetValues", ANY.to_any([speed]))
+
+        # socket
+        self.skt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.skt.connect((self.ipaddr_str, self.port))
+
+        # Get authority
+        self.get_authority()
+
+        return
+
+
+    def servo_on(self):
+        self.whole_body.Execute(u"ServoOn", ANY.to_any(None))
+
+
+    def start_task(self):
+        self.task.Execute(u"Start", ANY.to_any([[ "mode", CORBA.Any(CORBA.TC_long,2) ]]))
 
 
     # Acquire API Authority
@@ -54,10 +99,17 @@ class NextageNXAInterface():
         self.controller.Execute("ReleaseAuthority", ANY.to_any(None))
 
 
+    # variables
+    def write_var(self, var, val):
+        msg = str(var)+":"+str(val)
+        self.skt.send(msg.encode("utf-8"))
+        print(f"sent '{msg}'")
+        return
+
+
     def get_variable(self, variable_name='v_name'):
         s = "@GLOBAL_VARS/" + variable_name
         tv_name = self.task.GetVariable(s, "")  # task_variable_name
-
         value = ANY.from_any(tv_name.Execute("GetValues", ANY.to_any(None)))[0]
 
         # print
@@ -83,13 +135,7 @@ class NextageNXAInterface():
 
 
 if __name__ == "__main__":
-    ip="192.168.0.23"
-    nx_if = NextageNXAInterface(nx_ip=ip, debug=True)
+    nx_ip="192.168.0.23"
+    nx_if = NextageNXAInterface(ip=nx_ip, debug=True)
+    nx_if.setup()
     nx_if.get_variable("nextage_status")
-    nx_if.get_variable("tma_status")
-    nx_if.get_variable("sample_set_counter")
-    nx_if.get_variable("sample_get_counter")
-    nx_if.get_variable("thickness_counter")
-    nx_if.get_variable("micrometer_counter")
-
-    nx_if.set_variable("thickness_counter", "4")
